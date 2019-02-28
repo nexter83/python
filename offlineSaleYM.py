514069dc02f4b60c3f5c80d491d4911f18617355
nexter83 <31773558+nexter83@users.noreply.github.com>
2019-02-28 14:50:35 +0300

    Create offlineSaleYM.py

---
+++ offlineSaleYM.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from datetime import date, timedelta
+import datetime
+import numpy as np
+import requests
+
+def uploadYm(counterId, token, file):
+    url = 'https://api-metrika.yandex.net/management/v1/counter/' + str(counterId) + '/offline_conversions/upload?client_id_type=USER_ID'
+
+    headers = {
+        'Authorization': 'OAuth ' + token
+    }
+    r = requests.post(url, files={'file':open('data.csv', 'r')}, headers=headers)
+
+
+def main():
+    counterId = '****'
+    token = '****'
+    file = 'data.csv'
+    dateStart = date.today() - timedelta(1)
+    dateStart = dateStart.strftime('%Y%m%d')
+    uploadYm(counterId,token,file)
+
+
+if __name__ == '__main__':
+    try:
+        main()
